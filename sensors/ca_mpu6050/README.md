541e19facb9938099a6f4ac3adfef866b9802d4b
Emiliano Borghi <eborghi@ekumenlabs.com>
2019-01-05 22:42:33 -0300

    Add MPU6050 node

---
+++ sensors/ca_mpu6050/README.md
@@ -0,0 +1,5 @@
+# MPU6050
+
+Interfacing Raspberry Pi with MPU6050:
+
+![Connection between RPi and MPU6050](media/wiring.png)
---
+++ sensors/ca_mpu6050/setup.py
@@ -0,0 +1,12 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+                packages=['MPU6050'],
+                package_dir={'': 'lib/mpu6050_lib'},
+                )
+
+setup(**setup_args)
