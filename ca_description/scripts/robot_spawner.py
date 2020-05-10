78953398e77084641a79e9d5ce957cdb07a0fbb6
Emiliano Borghi <emiliano.borghi@gmail.com>
2020-05-10 18:48:31 -0300

    Fix global costamp inflation layer (#174)
    * Modify navigation RViz config
    
    * Wait for robot parameters
    
    * Remap move_base goal topic in RViz
    
    * Remove output in the screen for Cartographer
    
    * Edit move_base params
    
    * Add Trajectory Rollout local planner
    
    * Changes in launch files
    
    * fixup! Wait for robot parameters
    
    * Fix dwa, and tweaks in costmaps obstacle layer (#178)
    
    Co-authored-by: serraramiro1 <53626539+serraramiro1@users.noreply.github.com>

--- ca_description/scripts/robot_spawner.py
+++ ca_description/scripts/robot_spawner.py
@@ -11,14 +11,16 @@ from tf.transformations import quaternion_from_euler
 class RobotSpawner(object):
 
   SPAWN_URDF_TOPIC = '/gazebo/spawn_urdf_model'
+  TIMEOUT_S = rospy.Duration(secs=10.0)
+  CHECK_RATE_HZ   = 5
 
   def __init__(self):
     rospy.init_node('robot_spawner')
-    self.ns = rospy.get_namespace()
+    ns = rospy.get_namespace()
 
     # Get robot index
     try:
-      index = re.findall('[0-9]+', self.ns)[0]
+      index = re.findall('[0-9]+', ns)[0]
     except IndexError:
       index = 0
     i = index
@@ -36,21 +38,29 @@ class RobotSpawner(object):
 
       # Robot information from robot_description
       robot_description_param = "/create{}/robot_description".format(i)
-      if rospy.has_param(robot_description_param):
-        msg.model_xml = rospy.get_param(robot_description_param)
 
-      msg.robot_namespace = self.ns
+      # Wait for params
+      pose_param = "{}/amcl".format(ns)
+      initial_time = rospy.Time.now()
+      while RobotSpawner.TIMEOUT_S > (rospy.Time.now() - initial_time):
+        if rospy.has_param(robot_description_param) and rospy.has_param(pose_param):
+          break
+        rospy.sleep(1/RobotSpawner.CHECK_RATE_HZ)
+
+      msg.model_xml = rospy.get_param(robot_description_param)
+
+      msg.robot_namespace = ns
 
       # Using pose from parameter server
       msg.initial_pose = Pose()
       msg.initial_pose.position.x = rospy.get_param(
-            "{}/amcl/initial_pose_x".format(self.ns), randint(-10, 10))
+            "{}/initial_pose_x".format(pose_param), randint(-10, 10))
       msg.initial_pose.position.y = rospy.get_param(
-            "{}/amcl/initial_pose_y".format(self.ns), randint(-10, 10))
+            "{}/initial_pose_y".format(pose_param), randint(-10, 10))
       msg.initial_pose.position.z = rospy.get_param(
-            "{}/amcl/initial_pose_z".format(self.ns), 0.)
+            "{}/initial_pose_z".format(pose_param), 0.)
       yaw = rospy.get_param(
-            "{}/amcl/initial_pose_a".format(self.ns), 0.)
+            "{}/initial_pose_a".format(pose_param), 0.)
       q = quaternion_from_euler(0, 0, yaw)
       msg.initial_pose.orientation = Quaternion(q[0], q[1], q[2], q[3])
 
