fbbbedab8322000670dc52281293456a641e47b2
Emiliano Borghi <eborghiorue@frba.utn.edu.ar>
2020-07-09 16:07:05 -0300

    Remove IMU from cartographer to fix mapping (#229)
    * Remove IMU from cartographer to fix mapping
    
    * Fix env flag for localization

--- navigation/ca_cartographer/config/localization_2d.lua
+++ navigation/ca_cartographer/config/localization_2d.lua
@@ -6,7 +6,7 @@ options = {
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame  = os.getenv("tf_prefix") ..     -- Frame id to track from map
-                    "/imu_link",
+                    "/base_footprint",
   published_frame = os.getenv("tf_prefix") ..     -- Odom frame to publish if `provide_odom_frame = true`. Even though
                     "/odom",                      -- are not used, all the options' parameter keys are mandatory
   odom_frame =      os.getenv("tf_prefix") ..     -- Frame where publish the odometry to if `provide_odom_frame=true`
@@ -32,7 +32,7 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
